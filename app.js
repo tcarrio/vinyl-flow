ce54a95bdee99156fb3fe20cc3898a4a90aead1b
Tom Carrio <tom@carrio.me>
2019-04-09 19:30:02 -0400

    Refactored Express/Handlebars code into src/

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ const app = express()
 const port = 3000
 
 app.engine("hbs", hbs.express4({
-  // partialsDir: path.join(__dirname, "views/partials"),
-  layoutsDir: path.join(__dirname, "views/layouts/"),
-  defaultLayout: path.join(__dirname, "views/layouts/default.hbs"),
+  // partialsDir: path.join(__dirname, "src/views/partials"),
+  layoutsDir: path.join(__dirname, "src/views/layouts/"),
+  defaultLayout: path.join(__dirname, "src/views/layouts/default.hbs"),
   extname: ".hbs",
 }));
 app.set("view engine", "hbs")
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "src", "views"));
 
-const viewRouter = require('./routes/views')
-const apiRouter = require('./routes/api')
+const viewRouter = require('./src/routes/views')
+const apiRouter = require('./src/routes/api')
 
 app.use(logger('dev'))
 app.use(express.json())
